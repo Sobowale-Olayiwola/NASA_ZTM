bae29512df74965d5e02bccec0e674c5bff9c175
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2021-10-14 11:51:11 +0100

    Initial Commit

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,169 @@
+const axios = require("axios");
+
+const launchesDatabase = require("./launches.mongo");
+const planets = require("../models/planet.mongo");
+const launches = new Map();
+
+const DEFAULT_FLIGHT_NUMBER = 100;
+// const launch = {
+//   flightNumber: 100, //flight_number
+//   mission: "Kepler Exploration X", //name
+//   rocket: "Explorer IS1", //rocket.name
+//   launchDate: new Date("December 27,2030"), //date_local
+//   target: "Kepler-442 b",
+//   customers: ["ZTM", "NASA"], //payload.customers for each payload
+//   upcoming: true, //upcoming
+//   success: true, //success
+// };
+
+// saveLaunch(launch);
+
+const SPACE_API_URL = "https://api.spacexdata.com/v4/launches/query";
+
+async function populateLaunches() {
+  const response = await axios.post(SPACE_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+
+  if (response.status !== 200) {
+    console.log("Problem downloading launch data");
+    throw new Error("Launch data download failed");
+  }
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+    console.log(`${launch.flightNumber} ${launch.mission} ${launch.customers}`);
+
+    await saveLaunch(launch);
+  }
+}
+
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  });
+  console.log("Downloading launch data...");
+  if (firstLaunch) {
+    console.log("Launch data already loaded");
+    return;
+  } else {
+    await populateLaunches();
+  }
+}
+
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter);
+}
+async function existsLaunchWithId(launchId) {
+  return await findLaunch({
+    flightNumber: launchId,
+  });
+}
+async function getLastestFlightNumber() {
+  const lastestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
+  if (!lastestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return lastestLaunch.flightNumber;
+}
+
+async function getAllLaunches(skip, limit) {
+  return await launchesDatabase
+    .find(
+      {},
+      {
+        _id: 0,
+        __v: 0,
+      }
+    )
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
+}
+
+async function saveLaunch(launch) {
+  await launchesDatabase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
+}
+
+async function scheduleNewlaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+  const newFlightNumber = (await getLastestFlightNumber()) + 1;
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ["Zero to Mastery", "NASA"],
+    flightNumber: newFlightNumber,
+  });
+
+  await saveLaunch(newLaunch);
+}
+
+async function abortLaunchById(launchId) {
+  return await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+  // const aborted = launches.get(launchId);
+  // aborted.upcoming = false;
+  // aborted.success = false;
+  // return aborted;
+}
+
+module.exports = {
+  loadLaunchData,
+  existsLaunchWithId,
+  getAllLaunches,
+  abortLaunchById,
+  scheduleNewlaunch,
+};
